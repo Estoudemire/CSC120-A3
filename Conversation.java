82b91964137f11c81fb803d6b84656f467715f24
Emma Stoudemire <Estoudemire@smith.edu>
2023-02-15 22:41:20 -0500

    Bug that skips first user input, but it works

--- Conversation.java
+++ Conversation.java
@@ -9,6 +9,7 @@ class Conversation {
 
     //Defining variables I'll need
     int numRounds = 0;
+    String userResponse = " "; 
     //Defining an array list for the transcript 
     ArrayList<String> transcript = new ArrayList<String>(); 
 
@@ -24,14 +25,27 @@ class Conversation {
     System.out.println("How many rounds would you like?"); 
     numRounds = scanner.nextInt(); 
     System.out.println(" "); //Formatting
-    //Next, greeting the user
+    //Next, greeting the user & adding it to the transcript
     System.out.println(starter); 
-      //Add to transcript
+    transcript.add(starter); 
     //A for loop with however many rounds the user requested 
     for (int i = 0; i < numRounds; i++){
-       String userResponse = scanner.nextLine();
-
+      userResponse = scanner.nextLine();
+      transcript.add(userResponse); 
+      if (!userResponse.equals(" ")){
+        System.out.println(randResponseOne); 
+        transcript.add(randResponseOne); 
+      }
     }
+    System.out.println(ending); 
+    transcript.add(ending); 
 
+    //Printing the transcript
+    System.out.println(" "); //Formatting
+    System.out.println("TRANSCRIPT:");
+    for (int x = 0; x < transcript.size(); x++){
+        System.out.println(transcript.get(x)); 
+    }
+    
   }
 }
